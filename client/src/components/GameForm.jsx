8cba4eaae8cf7ac78f661d0f5a39e29f69cff009
killercho <ggeorgi60@gmail.com>
2022-03-19 12:03:55 +0200

    Added Popup style

--- client/src/components/GameForm.jsx
+++ client/src/components/GameForm.jsx
@@ -4,28 +4,35 @@ import "../styles/GameForm.css";
 import Heading from "./Heading";
 import Password from "./Password";
 import { useState } from "react";
+import TimerVariants from "../../../timer_variants.json";
+import ComboBox from "./ComboBox";
 
 const GameForm = ({ togglePopup }) => {
   const [input, setInput] = useState("");
-
-  const getGameModeOptions = () => {
-    let modes = [];
-    for (const mode in GameModes) {
-      modes.push(
-        <option key={mode} value={GameModes[mode]}>
-          {GameModes[mode]}
-        </option>
-      );
-    }
-    return modes;
-  };
+  const [timer, setTimer] = useState(TimerVariants[0]);
+  const [mode, setMode] = useState(GameModes.CLASSIC);
 
   return (
     <form>
       <Heading>Create game</Heading>
-      <Password password={input} setPassword={setInput} />
-      <input type="number" min="30" max="120" defaultValue={60} />
-      <select name="gameModes">{getGameModeOptions()}</select>
+      <div className="GameForm-password">
+        Password:
+        <Password password={input} setPassword={setInput} />
+      </div>
+      <ComboBox
+        options={TimerVariants}
+        currentOption={timer}
+        setCurrentOption={setTimer}
+      >
+        Time:
+      </ComboBox>
+      <ComboBox
+        options={Object.values(GameModes)}
+        currentOption={mode}
+        setCurrentOption={setMode}
+      >
+        Mode:
+      </ComboBox>
       <div className="Button-group">
         <Button onClick={togglePopup}>Back</Button>
         <Button>Create game</Button>
