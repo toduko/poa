40a97ea6c5ba1f32668dc6620ca1a55614eed7fd
Ga4o <gabrielshumanski@gmail.com>
2022-03-19 10:28:06 +0200

    Styled PageNotFound

--- client/src/components/PageNotFound.jsx
+++ client/src/components/PageNotFound.jsx
@@ -1,3 +1,13 @@
-const PageNotFound = () => <h1>404 Page Not Found</h1>;
+import "../styles/Heading.css";
+import Heading from "./Heading";
+import "../styles/PageNotFound.css";
+
+const PageNotFound = () => {
+  return (
+    <div className="PageNotFound">
+      <Heading>Error 404 Page Not Found</Heading>;
+    </div>
+  );
+};
 
 export default PageNotFound;
