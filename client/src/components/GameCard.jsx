9b302f124a52e06f0b169ec0678b037a8859ca66
Ga4o <gabrielshumanski@gmail.com>
2022-03-19 06:46:38 +0200

    Added Password component and styled game-related components

--- client/src/components/GameCard.jsx
+++ client/src/components/GameCard.jsx
@@ -1,15 +1,19 @@
+import { useState } from "react";
 import "../styles/GameCard.css";
 import Button from "./Button";
+import Password from "./Password";
 
 const GameCard = ({ mode, timer, password, uid }) => {
+  const [input, setInput] = useState("");
+
   return (
     <div className="GameCard rounded-corners">
       <p>Game ID: {uid}</p>
       <p>Time: {timer} seconds</p>
       <p>Mode: {mode}</p>
       <div className="GameCard-join">
-        <Button>Join</Button>
-        {password && <input type="text" placeholder="Password" />}
+        <Button disabled={input != password}>Join</Button>
+        {password && <Password password={input} setPassword={setInput} />}
       </div>
     </div>
   );
