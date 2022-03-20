ac4dadb6ec21e5b90bdd022be6bfa8586e484324
RylaD303 <alemak2002@gmail.com>
2022-03-20 14:29:59 +0200

    Fixed timer slowdown on canvas events

--- client/src/components/Room.jsx
+++ client/src/components/Room.jsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
-import { useRef, useState } from "react";
-import Canvas from "./Canvas";
+import { useEffect, useRef, useState } from "react";
+// import Canvas from "./Canvas";
 import ColorPicker from "./ColorPicker";
 import Heading from "./Heading";
 import "../styles/Room.css";
@@ -8,23 +8,97 @@ import Timer from "./Timer.jsx";
 import Button from "./Button";
 
 const Room = ({ socket, game, setGame }) => {
+  const [isDrawing, setIsDrawing] = useState(false);
   const [roomState, setRoomState] = useState(0);
   const [color, setColor] = useState("black");
-  const { roomId } = useParams();
-  const [image, setImage] = useState(new Image().src);
   const canvasRef = useRef(null);
+  let canvas, ctx;
+
+  const updateCanvasData = () => {
+    if (canvasRef.current) {
+      canvas = canvasRef.current;
+      ctx = canvas.getContext("2d");
+      ctx.lineCap = "round";
+      ctx.lineWidth = 5;
+      ctx.strokeStyle = color;
+    }
+  };
 
   socket.on("game-start", () => {
     setRoomState(roomState + 1);
   });
 
   const sendCanvasData = () => {
-    socket.emit("send-image-data", { gameID: game.id, image });
+    updateCanvasData();
+
+    socket.emit("send-image-data", {
+      gameID: game.id,
+      image: canvas.toDataURL(),
+    });
+
     changeRoomState();
   };
 
   const changeRoomState = () => {
     setRoomState(roomState + 1);
+  };
+
+  socket.on("receive-image-data", (images) => {
+    updateCanvasData();
+
+    const canvasImages = images.map((image) => {
+      const img = new Image(300, 300);
+      img.src = image;
+      return img;
+    });
+
+    canvasImages[0].onload = () => {
+      ctx.drawImage(canvasImages[0], 0, 0, 300, 300);
+    };
+    canvasImages[1].onload = () => {
+      ctx.drawImage(canvasImages[1], 300, 0, 300, 300);
+    };
+    canvasImages[2].onload = () => {
+      ctx.drawImage(canvasImages[2], 0, 300, 300, 300);
+    };
+    canvasImages[3].onload = () => {
+      ctx.drawImage(canvasImages[3], 300, 300, 300, 300);
+    };
+  });
+
+  useEffect(() => {
+    if (roomState == 1) {
+      updateCanvasData();
+    }
+  }, [color]);
+
+  const startDrawing = ({ nativeEvent }) => {
+    updateCanvasData();
+    const { offsetX, offsetY } = nativeEvent;
+    ctx.beginPath();
+    ctx.moveTo(offsetX, offsetY);
+    setIsDrawing(true);
+  };
+
+  const endDrawing = () => {
+    updateCanvasData();
+    ctx.closePath();
+    setIsDrawing(false);
+  };
+
+  const draw = ({ nativeEvent }) => {
+    if (!isDrawing) {
+      return;
+    }
+    updateCanvasData();
+    const { offsetX, offsetY } = nativeEvent;
+    ctx.lineTo(offsetX, offsetY);
+    ctx.stroke();
+  };
+
+  const handleClick = () => {
+    updateCanvasData();
+    ctx.clearRect(0, 0, width, height);
   };
 
   if (roomState === 0) {
@@ -35,30 +109,36 @@ const Room = ({ socket, game, setGame }) => {
     );
   } else if (roomState === 1) {
     return (
-      <div className="Room">
-        <Heading>Room {roomId}</Heading>
+      <>
         <Timer initialSeconds={2} timerOverHandler={sendCanvasData} />
         <ColorPicker setColor={setColor} activeColor={color} />
-        <Canvas
+        <Button onClick={handleClick}>Clear</Button>
+        <br />
+        <canvas
           width={300}
           height={300}
-          color={color}
-          canvasRef={canvasRef}
-          setImage={setImage}
-          socket={socket}
-          drawable
-          clear
+          onMouseDown={startDrawing}
+          onMouseUp={endDrawing}
+          onMouseMove={draw}
+          ref={canvasRef}
+          style={{
+            border: "1px solid black",
+            margin: "auto",
+          }}
         />
-      </div>
+      </>
     );
   } else if (roomState === 2) {
     return (
       <>
-        <Canvas
+        <canvas
           width={600}
           height={600}
-          canvasRef={canvasRef}
-          socket={socket}
+          ref={canvasRef}
+          style={{
+            border: "1px solid black",
+            margin: "auto",
+          }}
         />
         <Button onClick={() => window.location.reload(false)}>Leave</Button>
       </>
